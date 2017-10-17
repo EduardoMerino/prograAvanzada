d8fcc4c2d543da78e4551cb533bedca06d19cecd
Eduardo Merino <A01630088@itesm.mx>
2017-10-17 15:13:58 -0500

    mini proyecto 2 fix

--- single_number/main.c
+++ single_number/main.c
@@ -9,6 +9,10 @@ int main(int argc, char **argv) {
   printf("Single number.\n");
   printf("Give me the size of the array: ");
   scanf("%d", &inputSize);
+  if(inputSize == 0 || !(inputSize % 2)){
+    printf("the size must be of an odd number.\n");
+    return 0;
+  }
   uniqueSize = ((inputSize -1 )/ 2) + 1;
   int inputArr[inputSize];
   memset(inputArr, 0, inputSize);
@@ -16,6 +20,10 @@ int main(int argc, char **argv) {
   for(i = 0; i < inputSize; i++){
     printf("Give me the number for position %d :", i);
     scanf("%d", &temp);
+    if(temp == 0){
+      printf("The array can't contain 0. \n");
+      return 0;
+    }
     inputArr[i] = temp;
   }
 
