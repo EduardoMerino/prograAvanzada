868a20ce6ba339809a88ae3cca90e5cb389d0c0b
Eduardo Merino <A01630088@itesm.mx>
2017-11-02 20:31:36 -0600

    semaphore

--- thread/main.c
+++ thread/main.c
@@ -2,19 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <dispatch/dispatch.h>
 #define NUM_THREADS     100
 
 unsigned int total;
+dispatch_semaphore_t sem1;
 
 void *Add100(void *threadid){
   long tid;
   tid = (long)threadid;
   unsigned int localTotal;
   printf("Adding 100 from thread #%ld!\n", tid + 1);
+  dispatch_semaphore_wait(sem1, DISPATCH_TIME_FOREVER);
   localTotal = total;
   localTotal += 100;
-  sleep(tid%3);
   total = localTotal;
+  dispatch_semaphore_signal(sem1);
   pthread_exit(NULL);
 }
 
@@ -30,6 +33,7 @@ int main (int argc, char *argv[]){
    int rc;
    long t;
    total = 0;
+   sem1 = dispatch_semaphore_create(1);
    printf("Initial Total: %d\n", total);
    for(t=0; t<NUM_THREADS; t++){
       printf("In main: creating thread %ld\n", t + 1);
