2cb968fa3424cd6b33f8850b484266d8e4f397b9
Eduardo Merino <A01630088@itesm.mx>
2017-11-09 19:13:40 -0600

    Eating Phils complete

--- thread/main.c
+++ thread/main.c
@@ -5,9 +5,11 @@
 #include <dispatch/dispatch.h>
 #define NUM_THREADS     5
 
-//unsigned int total;
+//Semaphores:
 dispatch_semaphore_t forks[NUM_THREADS];
+dispatch_semaphore_t chairs;
 
+//The Eating Philosophers activities:
 void Thinking(int n){
   sleep(rand()%n);
 }
@@ -16,17 +18,22 @@ void Eating(int n){
   sleep(rand()%n);
 }
 
+//
 void *LifeStyle(void *threadid){
   long tid;
   tid = (long)threadid;
   while(1){
     Thinking(3);
     printf("Im the phil #%ld, I'm hungry \n", tid + 1);
+
+    dispatch_semaphore_wait(chairs, DISPATCH_TIME_FOREVER);
     dispatch_semaphore_wait(forks[tid], DISPATCH_TIME_FOREVER);
     dispatch_semaphore_wait(forks[(tid +1) % NUM_THREADS], DISPATCH_TIME_FOREVER);
     Eating(4);
+
     dispatch_semaphore_signal(forks[tid]);
     dispatch_semaphore_signal(forks[(tid +1) % NUM_THREADS]);
+    dispatch_semaphore_signal(chairs);
     printf("Im the phil #%ld, I'm thinking\n", tid + 1);
   }
 }
@@ -57,12 +64,11 @@ int main (int argc, char *argv[]){
    pthread_t threads[NUM_THREADS];
    int rc;
    long t;
-   //total = 0;
-   //sem1 = dispatch_semaphore_create(1);
-   //printf("Initial Total: %d\n", total);
+
    for(t=0; t<NUM_THREADS; t++){
      forks[t] = dispatch_semaphore_create(1);
    }
+  chairs = dispatch_semaphore_create(NUM_THREADS / 2);
    for(t=0; t<NUM_THREADS; t++){
       printf("In main: creating thread %ld\n", t + 1);
       rc = pthread_create(&threads[t], NULL, LifeStyle, (void *)t);
