a3a26daaa18dedf4c8b1a2076ec47c85fd8775f6
Eduardo Merino <A01630088@itesm.mx>
2017-11-16 19:39:25 -0600

    socket chat

---
+++ chat/chat_client/chat_client.c
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "chat_client.h"
+
---
+++ chat/chat_server/test_chat_server.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "minunit.h"
+#include "chat_server.h"
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+#define RESET "\033[0m"
+
+int testsRun = 0;
+
+static char * testUnit() {
+  muAssert("error, testUnit 1 != 1", 1 == 1);
+  return 0;
+}
+
+static char * allTests() {
+  muRunTest(testUnit);
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  char *result = allTests();
+  if (result != 0) {
+    printf("        __       _\n");
+    printf("     __/~~\\-- _ | \\ \n");
+    printf("__- - {            \\ \n");
+    printf("     /             \\  xx\n");
+    printf("    /       ;X    X }\n");
+    printf("    |              ;\n");
+    printf("                   |  \n");
+    printf("       \\_       (..)\n");
+    printf("         -_ _ _ /\n");
+    printf("           /\n");
+    printf("          /\n");
+    printf(KRED "✗ %s \n" RESET, result);
+  }
+  else {
+    printf("        __       _\n");
+    printf("     __/~~\\-- _ | \\ \n");
+    printf("__- - {            \\ \n");
+    printf("     /             \\  !!\n");
+    printf("    /       ;^    ^ }\n");
+    printf("    |              ;\n");
+    printf("                   |  \n");
+    printf("       \\_       (..)\n");
+    printf("         -_ _ _ /\n");
+    printf("           /\n");
+    printf("          /\n");
+    printf(KGRN " ✓ ALL TESTS PASSED \n" RESET);
+  }
+  printf("Tests run: %d\n", testsRun);
+  return result != 0;
+}
